23f33419c6a67e82c026d3a8915232386734d9fc
KristiyanVachev <typhon04@gmail.com>
2017-02-20 19:28:04 +0200

    Added tests for TourneyService

---
+++ BeerPong.Tests/Data/RepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Entity;
+using BeerPong.Data;
+using BeerPong.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace BeerPong.Tests.Data
+{
+    [TestFixture]
+    public class RepositoryTests
+    {
+        [TestCase(4)]
+        [TestCase(0)]
+        public void GetById_ShouldCallSetFind_WhenCalled(int id)
+        {
+            //Arrange
+            var setMock = new Mock<DbSet<Tourney>>();
+            var dbContext = new Mock<DbContext>();
+            dbContext.Setup(x => x.Set<Tourney>()).Returns(setMock.Object);
+            var repository = new Repository<Tourney>(dbContext.Object);
+
+            //Act
+            repository.GetById(id);
+
+            //Assert
+            setMock.Verify(x => x.Find(id), Times.Once);
+        }
+    }
+}
---
+++ BeerPong.Tests/Services/TourneyServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using BeerPong.Data.Contracts;
+using BeerPong.Factories;
+using BeerPong.Models;
+using BeerPong.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace BeerPong.Tests.Services
+{
+    [TestFixture]
+    public class TourneyServiceTests
+    {
+        [TestCase(2)]
+        [TestCase(24)]
+        public void GetById_ShouldCallRepositoryWithCorrectId(int id)
+        {
+            //Arrange
+            var mockFactory = new Mock<ITourneyFactory>();
+            var mockTourneyRepository = new Mock<IRepository<Tourney>>();
+            var mockUserRepository = new Mock<IRepository<User>>();
+            var mockPlayerRepository = new Mock<IRepository<Player>>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            var service = new TourneyService(
+                mockFactory.Object,
+                mockTourneyRepository.Object,
+                mockUserRepository.Object,
+                mockPlayerRepository.Object,
+                mockUnitOfWork.Object
+                );
+
+            //Act
+            service.GetById(id);
+
+            //Assert
+            mockTourneyRepository.Verify(x => x.GetById(id), Times.Once);
+        }
+    }
+}
