f46f591f3c4000fcfe57fb26c04b33c292ae0052
KristiyanVachev <typhon04@gmail.com>
2017-02-21 11:40:33 +0200

    Extracted interface of Tourney model.

---
+++ BeerPong.Models/Contracts/ITourney.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace BeerPong.Models.Contracts
+{
+    public interface ITourney
+    {           
+        int Id { get; set; }
+
+        string Name { get; set; }
+
+        string Status { get; set; }
+
+        string WinnerId { get; set; }
+
+        User Winner { get; set; }
+
+        ICollection<Player> Players { get; set; }
+
+        ICollection<Team> Teams { get; set; }
+
+        ICollection<Game> Games { get; set; }
+    }
+}
