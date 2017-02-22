72b55c804dcf72b32d42be4012bb502e6fea2815
KristiyanVachev <typhon04@gmail.com>
2017-02-22 10:18:39 +0200

    Added test for CreateTourneyPresenterTests

---
+++ BeerPong.Tests/MVP/Tourney/CreateTourneyPresenterTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BeerPong.MVP.Tourney.Create;
+using Moq;
+using NUnit.Framework;
+
+namespace BeerPong.Tests.MVP.Tourney
+{
+    [TestFixture]
+    public class CreateTourneyPresenterTests
+    {
+        [Test]
+        public void TestConstructor_PassServiceNull_ShouldThrowArgumentNullException()
+        {
+            var mockedView = new Mock<ICreateTourneyView>();
+
+            Assert.Throws<ArgumentNullException>(() => new CreateTourneyPresenter(mockedView.Object, null));
+        }
+    }
+}
