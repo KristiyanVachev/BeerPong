e61249a08fb52589db76bf208b19094aff641a5c
KristiyanVachev <typhon04@gmail.com>
2017-02-22 10:03:29 +0200

    Added MVP administration tests

--- BeerPong.Tests/MVP/Administration/TourneyPresenterTests.cs
+++ BeerPong.Tests/MVP/Administration/TourneyPresenterTests.cs
@@ -2,6 +2,8 @@
 using BeerPong.Models;
 using BeerPong.MVP;
 using BeerPong.MVP.Administration.Tourneys;
+using BeerPong.MVP.Tourney.Details;
+using BeerPong.MVP.Tourney.List;
 using BeerPong.Services.Contracts;
 using Moq;
 using NUnit.Framework;
@@ -32,6 +34,57 @@ namespace BeerPong.Tests.MVP.Administration
             var presenter = new TourneyPresenter(mockView.Object, mockService.Object, mockFactory.Object);
 
             Assert.IsNotNull(presenter);
+        }
+
+        [Test]
+        public void ViewMyInit_ShouldCallServiceGetProducts()
+        {
+            var mockView = new Mock<ITourneyView>();
+            mockView.Setup(v => v.Model).Returns(new TourneyListViewModel());
+
+            var mockFactory = new Mock<IViewModelFactory>();
+            var mockedService = new Mock<ITourneyService>();
+
+            var presenter = new TourneyPresenter(mockView.Object, mockedService.Object, mockFactory.Object);
+
+            mockView.Raise(v => v.MyInit += null, EventArgs.Empty);
+
+            mockedService.Verify(s => s.GetTourneys(), Times.Once);
+        }
+
+        [TestCase(2)]
+        [TestCase(52)]
+        public void ViewProductEdit_ShouldCallServiceGetById(int id)
+        {
+            var mockView = new Mock<ITourneyView>();
+            var mockFactory = new Mock<IViewModelFactory>();
+            var mockService = new Mock<ITourneyService>();
+
+            var presenter =
+                new TourneyPresenter(mockView.Object, mockService.Object, mockFactory.Object);
+
+            var model = new TourneyDetailsViewModel() { Id = id };
+            var args = new EditTourneyEventArgs(model);
+            mockView.Raise(x => x.EditTourney += null, args);
+
+            mockService.Verify(s => s.GetById(id), Times.Once);
+        }
+
+        [TestCase(4)]
+        [TestCase(12)]
+        public void TestViewProductDelete_ShouldCallServiceDeleteProduct(int id)
+        {
+            var mockView = new Mock<ITourneyView>();
+            var mockFactory = new Mock<IViewModelFactory>();
+            var mockService = new Mock<ITourneyService>();
+
+            var presenter =
+                new TourneyPresenter(mockView.Object, mockService.Object, mockFactory.Object);
+
+            var args = new DeleteTourneyEventArgs(id);
+            mockView.Raise(v => v.DeleteTourney += null, args);
+
+            mockService.Verify(s => s.DeleteTourney(id), Times.Once);
         }
     }
 }
